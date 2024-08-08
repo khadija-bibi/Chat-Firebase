38f9e27e2d4959b848d3de0689f7b39f58b5fb01
khadija <sub99way1122@gmail.com>
2024-08-08 12:11:41 +0500

    state managment continue

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./chatList.css";
 import AddUser from './addUser/AddUser';
-
+import {useUserStore} from "../../../lib/userStore"
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../lib/firebase';
 const Chatlist = () => {
   const [addMode, setAddMode] = useState(false);
+  const [chat, setChats] = useState([]);
+
+  const{currentUser} = useUserStore();
+
+  useEffect(()=>{
+    const unSub = onSnapshot(doc(db, "userchats", currentUser.id), (doc) => {
+      setChats(doc.data())
+  });
+  return ()=>{
+    unSub()
+  }
+  },[currentUser.id]);
+  console.log(chat);
+  
   return (
     <div className='chatList'>
         <div className="search">
